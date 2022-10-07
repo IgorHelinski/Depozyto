2a3429ee991133338b8219fb0c7dffef48ea3330
Zirael <70145364+igipigiel@users.noreply.github.com>
2022-10-07 15:24:09 +0200

    Dodano widok logowania i rejestracji
    Create SECURITY.md
    updated privacy policy

---
+++ Depozyto/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Depozyto.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Login/Register
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
